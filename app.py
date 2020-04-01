a71085884beafca1e353dcd532b62a493507a832
Thiago Nogueira <thiagonf10@gmail.com>
2020-04-01 15:27:37 -0300

    Adding url to get token from enquetes

--- app.py
+++ app.py
@@ -4,10 +4,18 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def hello_world():
-    token = get_access_token()
+
+@app.route('/edemocracia')
+def token_edemocracia():
+    token = get_access_token('edemocracia')
     return {"token": token}, 200
+
+
+@app.route('/enquetes')
+def token_enquetes():
+    token = get_access_token('enquetes')
+    return {"token": token}, 200
+
 
 if __name__ == '__main__':
     app.run()
--- service_account.py
+++ service_account.py
@@ -6,11 +6,18 @@ from oauth2client.service_account import ServiceAccountCredentials
 SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
 
 # The location of the key file with the key data.
-KEY_FILEPATH = 'json-key.json'
+KEY_EDEMOCRACIA = 'json-key-edemocracia.json'
+KEY_ENQUETES = 'json-key-enquetes.json'
+
 
 # Defines a method to get an access token from the ServiceAccount object.
+def get_access_token(account: str) -> str:
+    if account == 'edemocracia':
+        KEY_FILEPATH = KEY_EDEMOCRACIA
+    elif account == 'enquetes':
+        KEY_FILEPATH = KEY_ENQUETES
+    else:
+        KEY_FILEPATH = ''
 
-
-def get_access_token():
     return ServiceAccountCredentials.from_json_keyfile_name(
         KEY_FILEPATH, SCOPE).get_access_token().access_token
